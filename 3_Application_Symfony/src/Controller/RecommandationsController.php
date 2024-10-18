87fc21c13de83ce8a18806284303ed3a561304b6
TV00000 <thibault.vivier0@gmail.com>
2024-10-18 20:22:42 +0200

    19/10
    Premiers test d'affichage de données sur la page recommandation + tentative infructueuse de création de modal multiple

--- 3_Application_Symfony/src/Controller/RecommandationsController.php
+++ 3_Application_Symfony/src/Controller/RecommandationsController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\GamesRepository;
 
 class RecommandationsController extends AbstractController
 {
     #[Route('/recommandations', name: 'app_recommandations')]
-    public function index(): Response
+    public function index(GamesRepository $repository): Response
     {
+        $games = $repository->findBy(
+            ['review_score' => 90]
+        );
+        
         return $this->render('recommandations/index.html.twig', [
             'controller_name' => 'RecommandationsController',
+            'games' => $games,
         ]);
     }
 }

7100e2a18459ea2abd25cc5de33430922420cc61
TV00000 <thibault.vivier0@gmail.com>
2024-11-13 23:56:12 +0100

    13/11 23:55
    Fin du script R, enregistrement des graphes en local.
    Reste a integrer tout ça dans Symfony et on aura bien avancer

--- 2_Analyse_Exploiratoire/creation_graphes_ACP_STEAM.R
+++ 2_Analyse_Exploiratoire/creation_graphes_ACP_STEAM.R
@@ -5,9 +5,10 @@ library(DBI)
 library(ggplot2)
 library(cluster)
 library(fdm2id)
+library(plotly)
+library(htmlwidgets)
 
-
-## Connection à la base de données 
+## Connection Ã  la base de donn?es 
 
 # Connect to the MySQL database: con
 connexion <- DBI::dbConnect(RMySQL::MySQL(),
@@ -34,10 +35,10 @@ games <- dbReadTable(connexion, "games")
 data_games = as.data.frame(games)
 
 
-## Réalisation de l'ACP 
+## R?alisation de l'ACP 
 
-# mettre que les données quantitative dans l'ACP
-# variables utilisées :
+# mettre que les donn?es quantitative dans l'ACP
+# variables utilis?es :
 #  - copiesSold
 #  - revenue
 #  - recommandations
@@ -45,39 +46,43 @@ data_games = as.data.frame(games)
 data_games.pca = PCA(data_games[,c(15:19,21)])
 
 # enregistrement des graphes de l'ACP : 
-# - cercles des corrélations
+# - cercles des corr?lations
 # - projection sur le premier plan factoriel
 # - renvoyer aussi l'inertie totale du premier plan factoriel
 graphe_ACP_cercle_cor = plot(data_games.pca, type = "cor")
+ggsave("graphe_ACP_cercle_cor.png", plot=graphe_ACP_cercle_cor)
+
 graphe_ACP_projection_points = plot(data_games.pca)
+ggsave("graphe_ACP_projection_points.png", plot=graphe_ACP_projection_points)
+
 
 # inertie
 inertie_premier_plan_factoriel = data_games.pca$eig[2,3]
 
 
-## Application du K-MEANS sur les coordonnées de l'ACP
+## Application du K-MEANS sur les coordonn?es de l'ACP
 
-# détermination du nombre de cluster optimal
+# dÃ©termination du nombre de cluster optimal
 nb_cluster_opti = fdm2id::kmeans.getk(data_games.pca$ind$coor)
 
 # application du kmeans
 data_games_ACP_kmeans = fdm2id::KMEANS(data_games.pca$ind$coord, k=nb_cluster_opti)
 
 
-# recupération individuellement des données des clusters 
+# recup?ration individuellement des donn?es des clusters 
 
-# creation d'un dataframe vide pour accueillir les données de la table games 
-# avec leur nouvelle var désignant leur cluster associé.
+# creation d'un dataframe vide pour accueillir les donn?es de la table games 
+# avec leur nouvelle var d?signant leur cluster associ?.
 data_games_clust = data.frame(Dim1=numeric(), Dim2=numeric(), Dim3=numeric(), Dim4=numeric(), Dim5=numeric(), Dim6=numeric(), cluster=factor())
 
 for (i in 1:nb_cluster_opti) {
-  # récuperation de tous les index de la table de données du premier cluster
+  # r?cuperation de tous les index de la table de donn?es du premier cluster
   index_data_clust = as.vector(which(data_games_ACP_kmeans$cluster == i))
   
-  # extraction des données à partir de l'index et de la table originale.
+  # extraction des donn?es ? partir de l'index et de la table originale.
   data_games_clust_temp = as.data.frame(data_games.pca$ind$coord[index_data_clust,])
   
-  # ajout de la colonne associée au cluster
+  # ajout de la colonne associ?e au cluster
   data_games_clust_temp[, "cluster"] = i
   
   # fusion avec le dataset de resultat.
@@ -91,32 +96,9 @@ for (i in 1:nb_cluster_opti) {
 data_games.pca$ind$coord=as.matrix(data_games_clust)
 
 
-graphe_ACP_projection_points = plot.PCA(data_games.pca$ind$coord, axes = c(1,2), col.ind=data_games.pca$ind$coord[,"cluster"])
-
-
 data_games_clust =data_games_clust[,c(1,2,7)]
 
-## creation d'un nouveau graphe coloré en fonction du cluster déterminé avec KMEANS.
-graphe_ACP_projection_points_colored = plot(x = data_games_clust$Dim.1, y=data_games_clust$Dim.2, col = data_games_clust$cluster)
 
-
-
-autoplot()
-autoplot(data_games_clust, frame = TRUE, frame.type = 'norm')
-
-
-
-
-library(prcomp)
-library(ggfortify)
-
-pca_res <- prcomp(data_games[,c(15:19,21)], scale = TRUE)
-autoplot(pca_res, data = data_games_clust, colour = "cluster",  loadings = TRUE, loadings.label = TRUE)
-
-
-
-
-library(plotly)
 
 
 
@@ -130,8 +112,10 @@ fig <- plot_ly(data_games_clust, x = ~Dim.1, y = ~Dim.2, color = ~cluster, color
       title = "1"))
 for (i in 1:nrow(data_games.pca$var$coord)){
   fig <- fig %>%
-    add_segments(x = 0, xend = 20*data_games.pca$var$coord[i,"Dim.1"], y = 0, yend = 20*data_games.pca$var$coord[i,"Dim.2"], line = list(color = 'black'),inherit = FALSE, showlegend = FALSE) %>%
-    add_annotations(x=20*data_games.pca$var$coord[i,"Dim.1"], y=20*data_games.pca$var$coord[i,"Dim.2"], ax = 0, ay = 0,text = rownames(data_games.pca$var$coord)[i], xanchor = 'center', yanchor= 'bottom')
+    add_segments(x = 0, xend = 30*data_games.pca$var$coord[i,"Dim.1"], y = 0, yend = 30*data_games.pca$var$coord[i,"Dim.2"], line = list(color = 'black'),inherit = FALSE, showlegend = FALSE) %>%
+    add_annotations(x=30*data_games.pca$var$coord[i,"Dim.1"], y=30*data_games.pca$var$coord[i,"Dim.2"], ax = 0, ay = 0,text = rownames(data_games.pca$var$coord)[i], xanchor = 'center', yanchor= 'bottom')
 }
 
-fig
+
+# Sauvegarde du plot -> dans un objet HTML (particularitÃ© de plotly)
+htmlwidgets::saveWidget(as_widget(fig), "graphe_cluster_projection_acp.png.html")

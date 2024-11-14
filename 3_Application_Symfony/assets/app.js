f1039316191ab5c31af2fe65639ab49698baf421
XeroxBTW <mathis.carvalho1703@gmail.com>
2024-11-14 14:18:17 +0100

    Update app.js

--- 3_Application_Symfony/assets/app.js
+++ 3_Application_Symfony/assets/app.js
@@ -6,7 +6,11 @@ import './bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './vendor/bootstrap/dist/css/bootstrap.min.css';
-import './styles/app.css';
+/*import './styles/app.css';*/
+import './styles/analysis.css';
+import './styles/dashbord.css';
+import './styles/general.css';
+import './styles/recommandations.css';
 /*import './styles/potentielnewcss.css';*/
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');

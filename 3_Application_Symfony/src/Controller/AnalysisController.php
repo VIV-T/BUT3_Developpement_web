12edee4cc019ec1ef508b37923c8317f2a99a6b0
TV00000 <thibault.vivier0@gmail.com>
2024-11-20 17:58:31 +0100

    Suite

--- 3_Application_Symfony/src/Controller/AnalysisController.php
+++ 3_Application_Symfony/src/Controller/AnalysisController.php
@@ -73,7 +73,7 @@ class AnalysisController extends AbstractController
         
         // Execution de tous les scripts R
         foreach($liste_scripts as $r_script){
-            $process = new Process(['.\Rscript.exe', $r_script]);
+            $process = new Process(['Rscript', $r_script]);
             if ($os === "Windows"){
                 $process->setWorkingDirectory("C:/Program Files/R/R-4.4.2/bin/x64");
             }

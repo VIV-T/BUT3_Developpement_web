9377ca054774ff4dd4403ad5576ec9a05dc26a92
TV00000 <thibault.vivier0@gmail.com>
2024-11-21 18:18:35 +0100

    21/11 18:18
    Modification script R ACP => afficher les graphes dans la pages analysis

--- 3_Application_Symfony/assets/RGraph/Analysis/creation_graphes_ACP_STEAM.R
+++ 3_Application_Symfony/assets/RGraph/Analysis/creation_graphes_ACP_STEAM.R
@@ -140,30 +140,31 @@ data_games_clust =data_games_clust[,c(1,2,7)]
 # - renvoyer aussi l'inertie totale du premier plan factoriel
 
 
-#graphe_ACP_cercle_cor = plot(data_games.pca, type = "cor")
-#ggsave("graphe_ACP_cercle_cor.png", plot=graphe_ACP_cercle_cor)
+graphe_ACP_cercle_cor = plot(data_games.pca, type = "cor")
+ggsave("graphe_ACP_cercle_cor.png", plot=graphe_ACP_cercle_cor)
 
-#graphe_ACP_projection_points = plot(data_games.pca)
-#ggsave("graphe_ACP_projection_points.png", plot=graphe_ACP_projection_points)
+graphe_ACP_projection_points = plot(data_games.pca)
+ggsave("graphe_ACP_projection_points.png", plot=graphe_ACP_projection_points)
 
 
 # si possible, rajouter inertie du premier plan factoriel (var inertie_premier_plan_factoriel) et l'ajouter au graphe.
 
-fig <- plot_ly(data_games_clust, x = ~Dim.1, y = ~Dim.2, color = ~cluster, colors = c('#636EFA','#EF553B'), type = 'scatter', mode = 'markers') %>% 
-  layout(
-    legend=list(title=list(text='color')),
-    plot_bgcolor = "#e5ecf6",
-    xaxis = list(
-      title = "0"),
-    yaxis = list(
-      title = "1")) 
-for (i in 1:nrow(data_games.pca$var$coord)){
-  fig <- fig %>%
-    add_segments(x = 0, xend = 30*data_games.pca$var$coord[i,"Dim.1"], y = 0, yend = 30*data_games.pca$var$coord[i,"Dim.2"], line = list(color = 'black'),inherit = FALSE, showlegend = FALSE) %>%
-    add_annotations(x=30*data_games.pca$var$coord[i,"Dim.1"], y=30*data_games.pca$var$coord[i,"Dim.2"], ax = 0, ay = 0,text = rownames(data_games.pca$var$coord)[i], xanchor = 'center', yanchor= 'bottom')
-}
+#fig <- plot_ly(data_games_clust, x = ~Dim.1, y = ~Dim.2, color = ~cluster, colors = c('#636EFA','#EF553B'), type = 'scatter', mode = 'markers') %>% 
+#  layout(
+#    legend=list(title=list(text='color')),
+#    plot_bgcolor = '#e5ecf6',
+#    xaxis = list(
+#      title = '0'),
+#    yaxis = list(
+#      title = '1')) 
+#for (i in 1:nrow(data_games.pca$var$coord)){
+#  fig <- fig %>%
+#    add_segments(x = 0, xend = 30*data_games.pca$var$coord[i,'Dim.1'], y = 0, yend = 30*data_games.pca$var$coord[i,'Dim.2'], line = list(color = 'black'),inherit = FALSE, showlegend = FALSE) %>%
+#    add_annotations(x=30*data_games.pca$var$coord[i,'Dim.1'], y=30*data_games.pca$var$coord[i,'Dim.2'], ax = 0, ay = 0,text = rownames(data_games.pca$var$coord)[i], xanchor = 'center', yanchor= 'bottom')
+#}
 
-fig
+#fig
+
 
 # Sauvegarde du plot -> dans un objet HTML (particularité de plotly)
-htmlwidgets::saveWidget(as_widget(fig), "graphe_cluster_projection_acp.html")
+#htmlwidgets::saveWidget(as_widget(fig), "graphe_cluster_projection_acp.html")
